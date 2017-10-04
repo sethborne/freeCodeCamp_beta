2d4dda15b2c562e9acb798e2d20c99eb2a53e477
Seth Borne <sethborne@gmail.com>
2017-10-04 08:07:58 -0700

    freeCodeCamp - Javascript Algorithms and Data Structures - 30_39 - First Commit

---
+++ 02_JavaScript_Algorithms_and_Data_Structures/A_Basic_JavaScript/36_Word_Blanks.js
@@ -0,0 +1,10 @@
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    // Your code below this line
+    var result = "The " + myAdjective + " " + myNoun + " looked around then " + myVerb + " " + myAdverb + " into the house.";
+    console.log(result);
+    // Your code above this line
+    return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
