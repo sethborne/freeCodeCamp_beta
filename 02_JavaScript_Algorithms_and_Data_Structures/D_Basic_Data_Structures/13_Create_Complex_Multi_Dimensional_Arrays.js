06997fe45ab7cd43a81360e503cf79ae58918609
Seth Borne <sethborne@gmail.com>
2017-10-06 20:30:25 -0700

    freeCodeCamp - Javascript Algorithms and Data Structures - Basic Data Structures - 10_19 - First Commit

---
+++ 02_JavaScript_Algorithms_and_Data_Structures/D_Basic_Data_Structures/13_Create_Complex_Multi_Dimensional_Arrays.js
@@ -0,0 +1,32 @@
+let myNestedArray = [
+    // change code below this line
+    ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+    //2
+    [
+        'loop', 'shift', 6, 7, 1000, 'method'
+    ],
+    //3
+    [
+        
+        ['concat', false, true, 'spread', 'array', 'deep']
+        
+    ],
+    
+    //4
+    [
+        
+        [
+            ['mutate', 1327.98, 'splice', 'slice', 'push', 'deeper']
+        ]
+        
+    ],
+    //5
+    [
+        [
+            [
+                ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth', 'deepest']
+            ]
+        ]
+    ]
+    // change code above this line
+];
