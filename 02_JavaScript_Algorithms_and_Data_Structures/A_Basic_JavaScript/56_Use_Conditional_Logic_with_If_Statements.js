d0b469d25bcc2b27f6280606ade3aa414c4bbcf5
Seth Borne <sethborne@gmail.com>
2018-06-04 14:25:20 -0700

    freeCodeCamp - Javascript Algorithms and Data Structures - 52a 56 98a - Update Commit

--- 02_JavaScript_Algorithms_and_Data_Structures/A_Basic_JavaScript/56_Use_Conditional_Logic_with_If_Statements.js
+++ 02_JavaScript_Algorithms_and_Data_Structures/A_Basic_JavaScript/56_Use_Conditional_Logic_with_If_Statements.js
@@ -11,9 +11,9 @@ function trueOrFalse(wasThatTrue) {
 
     // Only change code below this line.
     if (wasThatTrue) {
-        return "Yes, that was true"
+        return "Yes, that was true";
     } else {
-        return "No, that was false"
+        return "No, that was false";
     }
 
 
