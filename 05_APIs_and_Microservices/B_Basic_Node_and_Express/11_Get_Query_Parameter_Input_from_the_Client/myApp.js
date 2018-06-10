af69bf781fca78c0e5ab8c60942fe16767c1c043
Seth Borne <sethborne@gmail.com>
2018-06-09 19:56:05 -0700

    freeCodeCamp - Apis And Microservices Projects - Basic Node and Express - 10_13 - First Commit

---
+++ 05_APIs_and_Microservices/B_Basic_Node_and_Express/11_Get_Query_Parameter_Input_from_the_Client/myApp.js
@@ -0,0 +1,107 @@
+
+var express = require('express');
+var app = express();
+
+// --> 7)  Mount the Logger middleware here
+
+app.use(function(req, res, next){
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+})
+
+// --> 11)  Mount the body-parser middleware  here
+
+
+
+/** 1) Meet the node console. */
+
+console.log('Hello World');
+
+/** 2) A first working Express Server */
+
+// app.get('/', function(req, res){
+//     res.send('Hello Express');
+// })
+
+/** 3) Serve an HTML file */
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + "/views/index.html");
+})
+
+/** 4) Serve static assets  */
+
+app.use(express.static(__dirname + "/public"));
+
+/** 5) serve JSON on a specific route */
+
+// app.get('/json', function(req, res){
+//     res.json({"message": "Hello json"})
+// })
+
+/** 6) Use the .env file to configure the app */
+
+let uppercase = "upperscase"
+process.env.MESSAGE_STYLE=uppercase
+
+app.use('/json', (req, res) => {
+
+    if (process.env.MESSAGE_STYLE==='uppercase') {
+        console.log(process.env.MESSAGE_STYLE)
+        res.json({
+            "message": "HELLO JSON"
+        })
+    } else {
+        res.json({
+            "message": "Hello json"
+        })
+    }
+})
+
+
+/** 7) Root-level Middleware - A logger */
+//  place it before all the routes !
+
+
+/** 8) Chaining middleware. A Time server */
+
+app.get('/now', function(req, res, next){
+    req.time(new Date().toString());
+    next();
+}, function(req, res){
+    res.send({time: req.time})
+})
+
+/** 9)  Get input from client - Route parameters */
+
+app.get('/:word/echo', function(req, res){
+    let word = req.params.word;
+    let echo = req.params.echo;
+    res.send({echo: word})
+})
+
+/** 10) Get input from client - Query parameters */
+// /name?first=<firstname>&last=<lastname>
+
+app.get('/name', function(req, res){
+    // ?first=firstname&last=lastname
+    let first = req.query.first;
+    let last = req.query.last;
+    res.send({ name: `${first} ${last}`})
+})
+
+/** 11) Get ready for POST Requests - the `body-parser` */
+// place it before all the routes !
+
+
+/** 12) Get data form POST  */
+
+
+
+// This would be part of the basic setup of an Express app
+// but to allow FCC to run tests, the server is already active
+/** app.listen(process.env.PORT || 3000 ); */
+
+//---------- DO NOT EDIT BELOW THIS LINE --------------------
+
+module.exports = app;
