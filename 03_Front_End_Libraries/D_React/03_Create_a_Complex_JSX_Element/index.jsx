cea2cfae46aa3f0aa31023772def96915b5c83aa
Seth Borne <sethborne@gmail.com>
2018-06-09 19:56:45 -0700

    freeCodeCamp - Front End Libraries - React - 02_09 - First Commit

---
+++ 03_Front_End_Libraries/D_React/03_Create_a_Complex_JSX_Element/index.jsx
@@ -0,0 +1,10 @@
+// write your code here
+const JSX = <div>
+    <h1>Heading 1</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa eveniet voluptates corporis! Illo corrupti sed, quasi qui omnis, aliquid ipsam accusantium optio voluptate, sequi dolores.</p>
+    <ul>
+        <li>List Item 01</li>
+        <li>List Item 02</li>
+        <li>List Item 03</li>
+    </ul>
+</div>
---
+++ 03_Front_End_Libraries/D_React/08_Create_a_Stateless_Functional_Component/index.jsx
@@ -0,0 +1,7 @@
+const MyComponent = function () {
+    // change code below this line
+    return (
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad quibusdam explicabo ipsa ipsum expedita, maxime ex earum eos fugiat dolores quidem architecto minus. Minima quis rem aperiam, atque illum enim.</div>
+    )
+    // change code above this line
+}
