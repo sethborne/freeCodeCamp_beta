15ea69fb90bc7af42dc647f6d579e0f14a48eb05
Seth Borne <sethborne@gmail.com>
2018-06-10 08:37:09 -0700

    freeCodeCamp - Algorithms - 06 - Update Commit

--- 07_Coding_Interview_Prep/A_Algorithms/06_Implement_Selection_Sort.js
+++ 07_Coding_Interview_Prep/A_Algorithms/06_Implement_Selection_Sort.js
@@ -1,27 +1,33 @@
-function selectionSort(array) {
-    // change code below this line
-    let min = Infinity;
-    let minIndex;
+// Here we will implement selection sort. Selection sort works by selecting the minimum value in a list and swapping it with the first value in the list. It then starts at the second position, selects the smallest value in the remaining list, and swaps it with the second element. It continues iterating through the list and swapping elements until it reaches the end of the list. Now the list is sorted. Selection sort has quadratic time complexity in all cases.
+
+// Instructions: Write a function selectionSort which takes an array of integers as input and returns an array of these integers in sorted order from least to greatest.
+
+function selectionSort(inputArray) {
     let i = 0;
-    while(i < array.length){
-        for(let j = i; j < array.length; j += 1){
-            if(array[i] < min){
-                min = array[i];
-                minIndex = i;
+    while(i < inputArray.length){
+        console.log(inputArray[i]);
+        // inc at end
+        let min = inputArray[i]
+        let minIndex = i;
+        for(let j = i + 1; j < inputArray.length; j += 1){
+            if(inputArray[j] <= min){
+                min = inputArray[j]
+                minIndex = j
             }
+            console.log(`At Loop: ${i} | Inner Loop: ${j} | Current Min: ${min}`);
         }
-        if(minIndex !== i){
-            // swap
-            i += 1;
-        }
-        else{
-            // dont swap
-            i += 1;
-        }
+        // swap
+        let tempVal = inputArray[i];
+        inputArray[i] = min;
+        inputArray[minIndex] = tempVal;
+        
+        i += 1;
     }
-    // change code above this line
-    return array;
+    console.log(inputArray);
+    return inputArray;
 }
 
 // test array:
-// [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]
+selectionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92])
+
+// loop through array
